ea23f0c1fc47a2b442a1e08bdb07bbad914960aa
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2021-02-13 18:31:13 -0500

    styling and naming

--- app.js
+++ app.js
@@ -20,23 +20,24 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'assets/images/underwater tileable.png');
-  this.load.image('seaweed', 'assets/images/waterplant.png');
+  this.load.image('block', 'assets/images/waterplant.png');
   this.load.image('penguin', 'assets/images/lr_penguin2.png');
   this.load.image('whale', 'assets/images/whale.png');
 }
 
-var seaweed;
+var block;
+var penguin;
 
 function create() {
   this.add.image(400, 300, 'background');
 
-  seaweed = this.physics.add.staticGroup();
+  block = this.physics.add.staticGroup();
 
-  //middle seaweed
-  seaweed.create(470, 490, 'blocker').setScale(6).refreshBody();
-  //left seaweed
-  seaweed.create(250, 520, 'blocker').setScale(4).refreshBody();
-  seaweed.create(90, 420, 'penguin').setScale(2).refreshBody();
+  //middle block
+  block.create(470, 490, 'seaweed').setScale(6).refreshBody();
+  //left block
+  block.create(250, 520, 'seaweed').setScale(4).refreshBody();
+  block.create(90, 420, 'penguin').setScale(2).refreshBody();
   this.add.image(700, 300, 'whale').setScale(2).refreshBody();
 }
 function update() {}
