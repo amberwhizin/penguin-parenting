1d527c3462e140ee6f101316bfb012a374c76adf
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2021-02-14 13:18:05 -0500

    changed peguin tweeked a few things

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ function preload() {
   this.load.image('background', 'assets/images/underwater tileable.png');
   this.load.image('ground', 'assets/images/beach_sand.png');
   this.load.image('blocks', 'assets/images/waterplant.png');
-  this.load.spritesheet('penguin', 'assets/images/tux.png', {
+  this.load.spritesheet('penguin1', 'assets/images/penguin.png', {
     frameWidth: 32,
-    frameHeight: 48,
+    frameHeight: 32,
   });
   this.load.image('whale', 'assets/images/whale.png');
 }
@@ -40,31 +40,31 @@ function create() {
   this.add.image(400, 300, 'background');
 
   blocks = this.physics.add.staticGroup();
-  
+
   blocks.create(470, 490, 'seaweed').setScale(6).refreshBody();
   //left blocks
   blocks.create(250, 520, 'seaweed').setScale(4).refreshBody();
 
   // create sprite
-  player = this.physics.add.sprite(550, 250, 'penguin');
+  player = this.physics.add.sprite(200, 450, 'penguin1').setScale(1.5);
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
   this.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('penguin', { start: 0, end: 2 }),
+    frames: this.anims.generateFrameNumbers('penguin1', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1,
   });
   this.anims.create({
     key: 'turn',
-    frames: [{ key: 'penguin', frame: 4 }],
+    frames: [{ key: 'penguin1', frame: 4 }],
     frameRate: 20,
   });
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('penguin', { start: 5, end: 8 }),
+    frames: this.anims.generateFrameNumbers('penguin1', { start: 5, end: 7 }),
     frameRate: 10,
     repeat: -1,
   });
@@ -76,16 +76,16 @@ function create() {
 }
 function update() {
   if (cursors.left.isDown) {
-    player.setVelocityX(-160);
+    player.setVelocityX(-185);
     player.anims.play('left', true);
   } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
+    player.setVelocityX(185);
     player.anims.play('right', true);
   } else {
     player.setVelocityX(0);
     player.anims.play('turn');
   }
   if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+    player.setVelocityY(-360);
   }
 }
