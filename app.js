12803c38213815a0067710c7e3abf7c7a76140ab
amberwhizin <53347515+amberwhizin@users.noreply.github.com>
2021-02-13 20:36:05 -0500

    player collides with walls

--- app.js
+++ app.js
@@ -16,28 +16,33 @@ var config = {
   },
 };
 
+var block;
+var player;
+
 var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'assets/images/underwater tileable.png');
   this.load.image('block', 'assets/images/waterplant.png');
-  this.load.image('penguin', 'assets/images/lr_penguin2.png');
+  this.load.spritesheet('penguin', 'assets/images/lr_penguin2.png', {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
   this.load.image('whale', 'assets/images/whale.png');
 }
-
-var block;
-var penguin;
 
 function create() {
   this.add.image(400, 300, 'background');
 
   block = this.physics.add.staticGroup();
 
-  //middle block
   block.create(470, 490, 'seaweed').setScale(6).refreshBody();
   //left block
   block.create(250, 520, 'seaweed').setScale(4).refreshBody();
-  block.create(90, 420, 'penguin').setScale(2).refreshBody();
-  this.add.image(700, 300, 'whale').setScale(2).refreshBody();
+
+  player = this.physics.add.sprite(100, 450, 'penguin');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
 }
 function update() {}
